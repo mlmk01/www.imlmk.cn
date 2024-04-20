9e34c5dc6aed00e87db909c4f2980fd2ca47582f
漫蓝梦坤 <3217816663@qq.com>
2024-04-20 07:55:04 -0700

    Add files via upload

---
+++ uptime/config.js
@@ -0,0 +1,33 @@
+window.Config = {
+
+  // 显示标题
+  SiteName: '漫蓝梦坤云服务器监控',
+
+  // UptimeRobot Api Keys
+  // 支持 Monitor-Specific 和 Read-Only
+  ApiKeys: [
+    'ur2254418-aff011e6663652b9faa3e622',
+  ],
+
+  // 日志天数
+  CountDays: 60,
+
+  // 是否显示检测站点的链接
+  ShowLink: true,
+
+  // 导航栏菜单
+  Navi: [
+    {
+      text: '主页',
+      url: 'https://www.oikun.com/'
+    },
+    {
+      text: 'GitHub',
+      url: 'https://github.com/mlmk6698'
+    },
+    {
+      text: 'Blog',
+      url: 'https://blog.oikun.com/'
+    },
+  ],
+};
